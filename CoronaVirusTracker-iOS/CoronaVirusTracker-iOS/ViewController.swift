8338584a1a95c01e76988df0efab45822f99b976
Chen Su <martinsuchen3557@gmail.com>
2020-03-08 20:58:57 -0400

    Project init

---
+++ CoronaVirusTracker-iOS/CoronaVirusTracker-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaVirusTracker-iOS
+//
+//  Created by CHEN SU on 3/8/20.
+//  Copyright © 2020 CHEN SU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
